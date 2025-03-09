221a5df8e843a6a1d20bb76e343a78025560e6b2
galets <git5511@mx1.galets.net>
2025-03-09 11:03:28 -0400

    reopen port instead of holding on to it

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
+#include <time.h> 
 #include "modbus.hpp"
 #include "registerReader.hpp"
 #include "registers.hpp"
 #include "mqtt.hpp"
 #include "hass.hpp"
 
+void getAndPost(Mqtt& mqtt) {
+    static time_t lastMetadataUpdate = 0;
+    const time_t secondsToForceMetadataUpdate = 60 * 60 * 2; // 2 hours
+
+    ModbusReader modbusReader("/dev/ttyUSB0", 19200, 'N', 8, 1, 1);
+    RegisterReader registersReader(modbusReader, RegisterType::INPUT);
+    RegisterReader holdingRegistersReader(modbusReader, RegisterType::HOLDING);
+    Registers r(registersReader, holdingRegistersReader);
+    HassDevice device(r);
+    HassInverter inverter(r, mqtt, device);
+
+    time_t now = time(0);
+    if (now > secondsToForceMetadataUpdate + lastMetadataUpdate) {
+        inverter.postDiscovery();
+        lastMetadataUpdate = now;
+    }
+
+    inverter.postValues();
+}
+
+
 int main()
 {
     try {
-        ModbusReader modbusReader("/dev/ttyUSB0", 19200, 'N', 8, 1, 1);
-        RegisterReader registersReader(modbusReader, RegisterType::INPUT);
-        RegisterReader holdingRegistersReader(modbusReader, RegisterType::HOLDING);
-        Registers r(registersReader, holdingRegistersReader);
-        HassDevice device(r);
         Mqtt mqtt;
-        HassInverter inverter(r, mqtt, device);
-    
-        inverter.postDiscovery();
-
         while (true) {
-            inverter.postValues();
-            registersReader.dropCaches();
-            holdingRegistersReader.dropCaches();
+            getAndPost(mqtt);
             std::this_thread::sleep_for(std::chrono::seconds(10));
         }
     
